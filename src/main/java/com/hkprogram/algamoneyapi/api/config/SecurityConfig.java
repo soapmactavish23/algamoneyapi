26e80ed588f96d20eb4c6eaf91d0b3745682661a
HKprogram <ricknogueira1231@gmail.com>
2021-02-23 17:48:28 -0300

    23-02

--- src/main/java/com/hkprogram/algamoneyapi/api/config/SecurityConfig.java
+++ src/main/java/com/hkprogram/algamoneyapi/api/config/SecurityConfig.java
@@ -9,22 +9,28 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
+		auth.inMemoryAuthentication()
+		.withUser("admin")
+		.password("{noop}admin")
+		.roles("ROLES");
 	}
-
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/categorias").permitAll()
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().csrf().disable();
+		.anyRequest()
+		.authenticated().and()
+		.httpBasic().and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.csrf().disable();
 	}
+	
+	
 	
 }
